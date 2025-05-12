998c5d53dbf964cad2f2a815cb914ba8f1b6cb70
MINLEE-HO <134053429+MINLEE-HO@users.noreply.github.com>
2025-05-13 00:57:27 +0900

    [Bronze V] Title: N 찍기, Time: 840 ms, Memory: 34008 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] argv){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i=1; i<=n; i++) {
+            System.out.println(i);
+        }
+    }
+}
